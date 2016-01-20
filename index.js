ff2fb8a0cf48a6f770569f570ea56896e946b59b
Kenan Yildirim <kenan@kenany.me>
2016-01-20 15:25:36 -0500

    Use socket-colors-chance
    Closes https://github.com/KenanY/chromatic-orb/issues/4

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
+var socketColorsChance = require('socket-colors-chance');
 var factorial = require('factorial');
 
 var X = 22;
 
 function chromatic(opts) {
-  if (!opts.sockets || opts.sockets < 1 || opts.sockets > 6) {
-    throw new Error('expected: 0 < opts.sockets < 7');
+  var chance = socketColorsChance(opts);
+
+  var strength = opts.strength || 0;
+  var dexterity = opts.dexterity || 0;
+  var intelligence = opts.intelligence || 0;
+
+  if (strength > 0 && dexterity === 0 && intelligence === 0) {
+    strength += 32;
+  }
+  else if (strength === 0 && dexterity > 0 && intelligence === 0) {
+    dexterity += 32;
+  }
+  else if (strength === 0 && dexterity === 0 && intelligence > 0) {
+    intelligence += 32;
   }
 
-  var red = opts.red || 0;
-  var green = opts.green || 0;
-  var blue = opts.blue || 0;
+  var div = strength + dexterity + intelligence + 3 * X;
 
-  if (red < 0 || green < 0 || blue < 0
-    || red + blue + green === 0
-    || red + blue + green > opts.sockets) {
-    throw new Error('invalid number of desired sockets');
-  }
-
-  function multinomial(red, green, blue, free, pos) {
-    pos = pos || 1;
-
-    if (free > 0) {
-      return (pos <= 1 ? multinomial(red + 1, green, blue, free - 1, 1) : 0)
-        + (pos <= 2 ? multinomial(red, green + 1, blue, free - 1, 2) : 0)
-        + multinomial(red, green, blue + 1, free - 1, 3);
-    }
-    return factorial(red + green + blue) / (factorial(red) * factorial(green)
-      * factorial(blue)) * Math.pow(rc, red) * Math.pow(gc, green)
-      * Math.pow(bc, blue);
-  }
+  var rc = (X + strength) / div;
+  var gc = (X + dexterity) / div;
+  var bc = (X + intelligence) / div;
 
   function bonus(free, dred, dgreen, dblue, red, green, blue, pos) {
     pos = pos || 1;
@@ -53,30 +49,7 @@ function chromatic(opts) {
       * Math.pow(bc, blue * 2);
   }
 
-  var strength = opts.strength || 0;
-  var dexterity = opts.dexterity || 0;
-  var intelligence = opts.intelligence || 0;
-
-  var sockets = opts.sockets - red - green - blue;
-
-  if (strength > 0 && dexterity === 0 && intelligence === 0) {
-    strength += 32;
-  }
-  else if (strength === 0 && dexterity > 0 && intelligence === 0) {
-    dexterity += 32;
-  }
-  else if (strength === 0 && dexterity === 0 && intelligence > 0) {
-    intelligence += 32;
-  }
-
-  var div = strength + dexterity + intelligence + 3 * X;
-
-  var rc = (X + strength) / div;
-  var gc = (X + dexterity) / div;
-  var bc = (X + intelligence) / div;
-
-  var chance = multinomial(red, green, blue || 0, sockets);
-  chance /= 1 - bonus(opts.sockets, red, green, blue);
+  chance /= 1 - bonus(opts.sockets, opts.red || 0, opts.green || 0, opts.blue || 0);
   return chance;
 }
 
